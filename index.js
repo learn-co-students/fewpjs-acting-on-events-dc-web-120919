7d69924d0520d813b3031701dadf9a66b19b414b
github username <github email address>
2020-01-27 14:18:57 -0500

    pause

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.bottom = "0px";
+
+function moveDodgerLeft() {
+    document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        let leftNumbers = dodger.style.left.replace("px", "");
+        let left = parseInt(leftNumbers, 10);
+        if (left < 0) {
+        dodger.style.left = `${left - 1}px`;
+        }
+    }
+
+  });
+}
+
+ function moveDodgerRight() {
+
+  document.addEventListener("keydown", function(f) {
+    if (f.key === "ArrowRight") {
+        let leftNumbers = dodger.style.left.replace("px", "");
+        let left = parseInt(leftNumbers, 10);
+        if (left < 0) {
+        dodger.style.left = `${left + 1}px`;
+        }
+    }
+
+  });
+}
+
+
+
+moveDodgerLeft();
+moveDodgerRight();
+
+
+
+
